9a9d62c821cd861c1c3d17bc2fa67f842d30f97f
Olena Shabronova <shabronova@ukr.net>
2023-02-28 05:55:03 -0800

    new project

---
+++ src/js/modules/slider/slider-main.js
@@ -0,0 +1,92 @@
+import Slider from "./slider";
+
+export default class MainSlider extends Slider {
+  constructor(btns) {
+    try {
+      super(btns);
+    } catch (e) {}
+  }
+
+  showSlides(n) {
+    try {
+      if (n > this.slides.length) {
+        this.slideIndex = 1;
+      }
+    } catch (e) {}
+
+    try {
+      if (n < 1) {
+        this.slideIndex = this.slides.length;
+      }
+    } catch (e) {}
+
+    try {
+      this.hanson.style.opacity = "0";
+
+      if (n == 3) {
+        this.hanson.classList.add("animated");
+        setTimeout(() => {
+          this.hanson.style.opacity = "1";
+          this.hanson.classList.add("slideInUp");
+        }, 3000);
+      } else {
+        this.hanson.classList.remove("slideInUp");
+      }
+    } catch (e) {}
+
+    try {
+      this.slides.forEach((slide) => {
+        slide.style.display = "none";
+      });
+    } catch (e) {}
+
+    this.slides[this.slideIndex - 1].style.display = "block";
+  }
+
+  plusSlides(n) {
+    try {
+      this.showSlides((this.slideIndex += n));
+    } catch (e) {}
+  }
+
+  bindTriggers() {
+    this.btns.forEach((item) => {
+      item.addEventListener("click", () => {
+        this.plusSlides(1);
+      });
+
+      item.parentNode.previousElementSibling.addEventListener("click", (e) => {
+        e.preventDefault();
+        this.slideIndex = 1;
+        this.showSlides(this.slideIndex);
+      });
+    });
+
+    document.querySelectorAll(".prevmodule").forEach((item) => {
+      item.addEventListener("click", (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        this.plusSlides(-1);
+      });
+    });
+
+    document.querySelectorAll(".nextmodule").forEach((item) => {
+      item.addEventListener("click", (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        this.plusSlides(1);
+      });
+    });
+  }
+
+  render() {
+    try {
+      try {
+        this.hanson = document.querySelector(".hanson");
+      } catch (e) {}
+
+      this.showSlides(this.slideIndex);
+      this.bindTriggers();
+    } catch (e) {}
+  }
+}
